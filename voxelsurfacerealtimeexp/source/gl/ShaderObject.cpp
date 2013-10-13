602b24a9dc86d16c923401b13bd7d723abcee34b
Andreas Reich <reichandreas@gmx.de>
2013-10-13 20:32:37 +0200

    nearly all shader metadata is now gathered by gl::ShaderObject

--- voxelsurfacerealtimeexp/source/gl/ShaderObject.cpp
+++ voxelsurfacerealtimeexp/source/gl/ShaderObject.cpp
@@ -1,6 +1,9 @@
-#include "PCH.h"
+﻿#include "PCH.h"
 #include "ShaderObject.h"
 #include "GLUtils.h"
+
+#include <GL/glew.h>
+
 #include <Foundation/io/FileSystem/FileReader.h>
 #include <Foundation/io/OSFile.h>
 #include <Foundation/Basics/Types/ArrayPtr.h>
@@ -14,9 +17,9 @@ namespace gl
   {
     for(Shader& shader : m_aShader)
     {
-      shader.m_ShaderObject = 0;
-      shader.m_sOrigin = "none";
-      shader.m_bLoaded = false;
+      shader.iGLShaderObject = 0;
+      shader.sOrigin = "none";
+      shader.bLoaded = false;
     }
   }
 
@@ -24,15 +27,15 @@ namespace gl
   {
     for(Shader& shader : m_aShader)
     {
-      if(shader.m_bLoaded)
-        glDeleteShader(shader.m_ShaderObject);
+      if(shader.bLoaded)
+        glDeleteShader(shader.iGLShaderObject);
     }
 
     if(m_ContainsAssembledProgram)
       glDeleteProgram(m_Program);
   }
 
-  ezResult ShaderObject::AddShaderFromFile(ShaderType type, const ezString& sFilename)
+  ezResult ShaderObject::AddShaderFromFile(ShaderType Type, const ezString& sFilename)
   {
     // open file
     ezFileReader file;
@@ -52,34 +55,34 @@ namespace gl
     file.Close();
 
 
-    return AddShaderFromSource(type, static_cast<ezArrayPtr<char>>(pData).GetPtr(), sFilename);
+    return AddShaderFromSource(Type, static_cast<ezArrayPtr<char>>(pData).GetPtr(), sFilename);
   }
 
   ezResult ShaderObject::AddShaderFromSource(ShaderType type, const ezString& pSourceCode, const ezString& sOriginName)
   {
     // create shader
     Shader& shader = m_aShader[static_cast<ezUInt32>(type)];
-    EZ_ASSERT(!shader.m_bLoaded, "Shader already loaded!");
-    shader.m_sOrigin = sOriginName;
+    EZ_ASSERT(!shader.bLoaded, "Shader already loaded!");
+    shader.sOrigin = sOriginName;
     switch (type)
     {
     case ShaderObject::ShaderType::VERTEX:
-      shader.m_ShaderObject = glCreateShader(GL_VERTEX_SHADER);
+      shader.iGLShaderObject = glCreateShader(GL_VERTEX_SHADER);
       break;
     case ShaderObject::ShaderType::FRAGMENT:
-      shader.m_ShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+      shader.iGLShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
       break;
     case ShaderObject::ShaderType::EVALUATION:
-      shader.m_ShaderObject = glCreateShader(GL_TESS_EVALUATION_SHADER);
+      shader.iGLShaderObject = glCreateShader(GL_TESS_EVALUATION_SHADER);
       break;
     case ShaderObject::ShaderType::CONTROL:
-      shader.m_ShaderObject = glCreateShader(GL_TESS_CONTROL_SHADER);
+      shader.iGLShaderObject = glCreateShader(GL_TESS_CONTROL_SHADER);
       break;
     case ShaderObject::ShaderType::GEOMETRY:
-      shader.m_ShaderObject = glCreateShader(GL_GEOMETRY_SHADER);
+      shader.iGLShaderObject = glCreateShader(GL_GEOMETRY_SHADER);
       break;
     case ShaderObject::ShaderType::COMPUTE:
-      shader.m_ShaderObject = glCreateShader(GL_COMPUTE_SHADER);
+      shader.iGLShaderObject = glCreateShader(GL_COMPUTE_SHADER);
       break;
 
     default:
@@ -90,16 +93,16 @@ namespace gl
 
     // compile shader
     const char* pSourceRaw = pSourceCode.GetData();
-    glShaderSource(shader.m_ShaderObject, 1, &pSourceRaw, NULL);	// attach shader code
-    glCompileShader(shader.m_ShaderObject);								    // compile
+    glShaderSource(shader.iGLShaderObject, 1, &pSourceRaw, NULL);	// attach shader code
+    glCompileShader(shader.iGLShaderObject);								    // compile
 
     // check result
     ezResult result = gl::Utils::CheckError("glCompileShader");
     if(result == EZ_SUCCESS)
-      shader.m_bLoaded = true;
+      shader.bLoaded = true;
 
     // log output
-    PrintShaderInfoLog(m_aShader[static_cast<ezUInt32>(type)].m_ShaderObject, sOriginName);					
+   // PrintShaderInfoLog(m_aShader[static_cast<ezUInt32>(type)].m_ShaderObject, sOriginName);					
 
     return result;
   }
@@ -109,6 +112,13 @@ namespace gl
   {
     EZ_ASSERT(!m_ContainsAssembledProgram, "ShaderObject contains already an assembled program.");
 
+    // clear meta information
+    m_iTotalProgramInputCount = 0;
+    m_iTotalProgramOutputCount = 0;
+    m_GlobalUniformInfo.Clear();
+    m_UniformBlockInfos.Clear();
+    m_ShaderStorageInfos.Clear();
+
     // Create shader program
     m_Program = glCreateProgram();	
 
@@ -116,9 +126,9 @@ namespace gl
     int numAttachedShader = 0;
     for(Shader& shader : m_aShader)
     {
-      if(shader.m_bLoaded)
+      if(shader.bLoaded)
       {
-        glAttachShader(m_Program, shader.m_ShaderObject);
+        glAttachShader(m_Program, shader.iGLShaderObject);
         ++numAttachedShader;
       }
     }
@@ -127,28 +137,127 @@ namespace gl
     // Link program
     glLinkProgram(m_Program);
 
-    // debug output
-    PrintProgramInfoLog(m_Program);
-
     // check
     if(gl::Utils::CheckError("CreateProgram") == EZ_FAILURE)
       return EZ_FAILURE;
     m_ContainsAssembledProgram = true;
 
-    // get uniform information
-    // todo: store anything you can get
-    // -> debugging
-    // -> automatic attaching of stuff
-    // http://www.opengl.org/wiki/Program_Introspection#Uniforms_and_blocks
+    // debug output
+    PrintProgramInfoLog(m_Program);
 
-
-    /* int iMaxUniformIndex;
-    glGetProgramiv(m_Program, GL_ACTIVE_UNIFORMS?, &iMaxUniformIndex);
-    ezDynamicArray<
-    glGetUniformIndices?(m_Program?, iMaxUniformIndex, const char ** uniformNames?, GLuint *uniformIndices?);
-    */
+    // get informations about the program
+    QueryProgramInformations();
 
     return EZ_SUCCESS;
+  }
+
+  void ShaderObject::QueryProgramInformations()
+  {
+    // query basic uniform & shader storage block infos
+    QueryBlockInformations(m_UniformBlockInfos, GL_UNIFORM_BLOCK);
+    QueryBlockInformations(m_ShaderStorageInfos, GL_SHADER_STORAGE_BLOCK);
+
+    // informations about uniforms ...
+    GLint iTotalNumUniforms = 0;
+    glGetProgramInterfaceiv(m_Program, GL_UNIFORM, GL_ACTIVE_RESOURCES, &iTotalNumUniforms);
+    const GLuint iNumQueriedUniformProps = 10;
+    const GLenum pQueriedUniformProps[] = { GL_NAME_LENGTH, GL_TYPE, GL_ARRAY_SIZE, GL_OFFSET, GL_BLOCK_INDEX, GL_ARRAY_STRIDE, GL_MATRIX_STRIDE, GL_IS_ROW_MAJOR, GL_ATOMIC_COUNTER_BUFFER_INDEX, GL_LOCATION  };
+    GLint pRawUniformBlockInfoData[iNumQueriedUniformProps];
+    for(int iBlock=0; iBlock<iTotalNumUniforms; ++iBlock)
+    {
+      // general data
+      glGetProgramResourceiv(m_Program, GL_UNIFORM, iBlock, iNumQueriedUniformProps, pQueriedUniformProps, iNumQueriedUniformProps, NULL, pRawUniformBlockInfoData);
+      UniformInfo uniformInfo;
+      uniformInfo.Type = static_cast<gl::ShaderVariableType>(pRawUniformBlockInfoData[1]);
+      uniformInfo.iArrayElementCount = static_cast<ezInt32>(pRawUniformBlockInfoData[2]);
+      uniformInfo.iBlockOffset = static_cast<ezInt32>(pRawUniformBlockInfoData[3]);
+      uniformInfo.iArrayStride = static_cast<ezInt32>(pRawUniformBlockInfoData[5]) * 4;
+      uniformInfo.iMatrixStride = static_cast<ezInt32>(pRawUniformBlockInfoData[6]);
+      uniformInfo.bRowMajor = pRawUniformBlockInfoData[7] > 0;
+      uniformInfo.iAtomicCounterbufferIndex = pRawUniformBlockInfoData[8];
+      uniformInfo.iLocation = pRawUniformBlockInfoData[9];
+
+      // name
+      GLint iActualNameLength = 0;
+      ezHybridArray<char, 64> RawName;
+      RawName.SetCount(pRawUniformBlockInfoData[0] + 1);
+      glGetProgramResourceName(m_Program, GL_UNIFORM, iBlock, RawName.GetCount(), &iActualNameLength, static_cast<ezArrayPtr<char>>(RawName).GetPtr());
+      RawName[iActualNameLength] = '\0';
+      uniformInfo.sName = static_cast<ezArrayPtr<char>>(RawName).GetPtr();
+
+      // where to store:
+      if(pRawUniformBlockInfoData[4] < 0)
+        m_GlobalUniformInfo.PushBack(uniformInfo);
+      else
+        m_UniformBlockInfos[pRawUniformBlockInfoData[4]].Variables.PushBack(uniformInfo);
+    }
+
+    // informations about shader storage variables 
+    GLint iTotalNumStorages = 0;
+    glGetProgramInterfaceiv(m_Program, GL_BUFFER_VARIABLE, GL_ACTIVE_RESOURCES, &iTotalNumStorages);
+    const GLuint iNumQueriedStorageProps = 10;
+    const GLenum pQueriedStorageProps[] = { GL_NAME_LENGTH, GL_TYPE, GL_ARRAY_SIZE, GL_OFFSET, GL_BLOCK_INDEX, GL_ARRAY_STRIDE, GL_MATRIX_STRIDE, GL_IS_ROW_MAJOR, GL_TOP_LEVEL_ARRAY_SIZE, GL_TOP_LEVEL_ARRAY_STRIDE };
+    GLint pRawStorageBlockInfoData[iNumQueriedStorageProps];
+    for(int iBlock=0; iBlock<iTotalNumStorages; ++iBlock)
+    {
+      // general data
+      glGetProgramResourceiv(m_Program, GL_BUFFER_VARIABLE, iBlock, iNumQueriedStorageProps, pQueriedStorageProps, iNumQueriedStorageProps, NULL, pRawStorageBlockInfoData);
+      BufferVariableInfo storageInfo;
+      storageInfo.Type = static_cast<gl::ShaderVariableType>(pRawStorageBlockInfoData[1]);
+      storageInfo.iArrayElementCount = static_cast<ezInt32>(pRawStorageBlockInfoData[2]);
+      storageInfo.iBlockOffset = static_cast<ezInt32>(pRawStorageBlockInfoData[3]);
+      storageInfo.iArrayStride = static_cast<ezInt32>(pRawStorageBlockInfoData[5]);
+      storageInfo.iMatrixStride = static_cast<ezInt32>(pRawStorageBlockInfoData[6]);
+      storageInfo.bRowMajor = pRawStorageBlockInfoData[7] > 0;
+      storageInfo.iTopLevelArraySize = pRawStorageBlockInfoData[8];
+      storageInfo.iTopLevelArrayStride = pRawStorageBlockInfoData[9];
+
+      // name
+      GLint iActualNameLength = 0;
+      ezHybridArray<char, 64> RawName;
+      RawName.SetCount(pRawStorageBlockInfoData[0] + 1);
+      glGetProgramResourceName(m_Program, GL_BUFFER_VARIABLE, iBlock, RawName.GetCount(), &iActualNameLength, static_cast<ezArrayPtr<char>>(RawName).GetPtr());
+      RawName[iActualNameLength] = '\0';
+      storageInfo.sName = static_cast<ezArrayPtr<char>>(RawName).GetPtr();
+
+      // where to store:
+       m_ShaderStorageInfos[pRawStorageBlockInfoData[4]].Variables.PushBack(storageInfo);
+    }
+
+    // other informations
+    glGetProgramInterfaceiv(m_Program, GL_PROGRAM_INPUT, GL_ACTIVE_RESOURCES, &m_iTotalProgramInputCount);
+    glGetProgramInterfaceiv(m_Program, GL_PROGRAM_OUTPUT, GL_ACTIVE_RESOURCES, &m_iTotalProgramOutputCount);
+  }
+
+  template<typename BufferVariableType>
+  void ShaderObject::QueryBlockInformations(ezDynamicArray<BufferInfo<BufferVariableType>>& BufferToFill, GLenum InterfaceName)
+  {
+    BufferToFill.Clear();
+
+    GLint iTotalNumBlocks = 0;
+    glGetProgramInterfaceiv(m_Program, InterfaceName, GL_ACTIVE_RESOURCES, &iTotalNumBlocks);
+    BufferToFill.SetCount(iTotalNumBlocks);
+
+    // gather infos about uniform blocks
+    const GLuint iNumQueriedBlockProps = 4;
+    const GLenum pQueriedBlockProps[] = { GL_NAME_LENGTH, GL_BUFFER_BINDING, GL_BUFFER_DATA_SIZE, GL_NUM_ACTIVE_VARIABLES };
+    GLint pRawUniformBlockInfoData[iNumQueriedBlockProps];
+    for(int iBlock=0; iBlock<iTotalNumBlocks; ++iBlock)
+    {
+      // general data
+      glGetProgramResourceiv(m_Program, InterfaceName, iBlock, iNumQueriedBlockProps, pQueriedBlockProps, iNumQueriedBlockProps, NULL, pRawUniformBlockInfoData);
+      BufferToFill[iBlock].iBufferBinding = pRawUniformBlockInfoData[1];
+      BufferToFill[iBlock].iBufferDataSizeByte = pRawUniformBlockInfoData[2] * sizeof(float);
+      BufferToFill[iBlock].Variables.Reserve(pRawUniformBlockInfoData[3]);
+
+      // name
+      GLint iActualNameLength = 0;
+      ezHybridArray<char, 64> RawName;
+      RawName.SetCount(pRawUniformBlockInfoData[0] + 1);
+      glGetProgramResourceName(m_Program, InterfaceName, iBlock, RawName.GetCount(), &iActualNameLength, static_cast<ezArrayPtr<char>>(RawName).GetPtr());
+      RawName[iActualNameLength] = '\0';
+      BufferToFill[iBlock].sName = static_cast<ezArrayPtr<char>>(RawName).GetPtr();
+    }
   }
 
   GLuint ShaderObject::GetProgram() const
@@ -163,15 +272,15 @@ namespace gl
     glUseProgram(m_Program);
   }
 
-  void ShaderObject::PrintShaderInfoLog(GLuint shader, const ezString& sShaderName) const
+  void ShaderObject::PrintShaderInfoLog(GLuint Shader, const ezString& sShaderName)
   {
 #ifdef EZ_COMPILE_FOR_DEVELOPMENT
     GLint infologLength = 0;
     GLsizei charsWritten  = 0;
 
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH,&infologLength);		
+    glGetShaderiv(Shader, GL_INFO_LOG_LENGTH,&infologLength);		
     ezArrayPtr<char> pInfoLog = EZ_DEFAULT_NEW_ARRAY(char, infologLength);
-    glGetShaderInfoLog(shader, infologLength, &charsWritten, pInfoLog.GetPtr());
+    glGetShaderInfoLog(Shader, infologLength, &charsWritten, pInfoLog.GetPtr());
     pInfoLog[charsWritten] = '\0';
     if(strlen(pInfoLog.GetPtr()) > 0)
     {
@@ -186,15 +295,15 @@ namespace gl
   }
 
   // Print information about the linking step
-  void ShaderObject::PrintProgramInfoLog(GLuint program) const
+  void ShaderObject::PrintProgramInfoLog(GLuint Program)
   {
 #ifdef EZ_COMPILE_FOR_DEVELOPMENT
     GLint infologLength = 0;
     GLsizei charsWritten  = 0;
 
-    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infologLength);		
+    glGetProgramiv(Program, GL_INFO_LOG_LENGTH, &infologLength);		
     ezArrayPtr<char> pInfoLog = EZ_DEFAULT_NEW_ARRAY(char, infologLength);
-    glGetProgramInfoLog(program, infologLength, &charsWritten, pInfoLog.GetPtr());
+    glGetProgramInfoLog(Program, infologLength, &charsWritten, pInfoLog.GetPtr());
     pInfoLog[charsWritten] = '\0';
 
     if(strlen(pInfoLog.GetPtr()) > 0)
--- voxelsurfacerealtimeexp/source/gl/ShaderObject.h
+++ voxelsurfacerealtimeexp/source/gl/ShaderObject.h
@@ -1,12 +1,131 @@
-#pragma once
+﻿#pragma once
 
 namespace gl
 {
+  enum class ShaderVariableType
+  {
+    FLOAT​ = GL_FLOAT,
+    FLOAT_VEC2​ = GL_FLOAT_VEC2,
+    FLOAT_VEC3​ = GL_FLOAT_VEC3,
+    FLOAT_VEC4​ = GL_FLOAT_VEC4,
+    DOUBLE​ = GL_DOUBLE,
+    DOUBLE_VEC2​ = GL_DOUBLE_VEC2,
+    DOUBLE_VEC3​ = GL_DOUBLE_VEC3,
+    DOUBLE_VEC4​ = GL_DOUBLE_VEC4,
+    INT​ = GL_INT,
+    INT_VEC2​ = GL_INT_VEC2,
+    INT_VEC3​ = GL_INT_VEC3,
+    INT_VEC4​ = GL_INT_VEC4,
+    UNSIGNED_INT​ = GL_UNSIGNED_INT,
+    UNSIGNED_INT_VEC2​ = GL_UNSIGNED_INT_VEC2,
+    UNSIGNED_INT_VEC3​ = GL_UNSIGNED_INT_VEC3,
+    UNSIGNED_INT_VEC4​ = GL_UNSIGNED_INT_VEC4,
+    BOOL​ = GL_BOOL,
+    BOOL_VEC2​ = GL_BOOL_VEC2,
+    BOOL_VEC3​ = GL_BOOL_VEC3,
+    BOOL_VEC4​ = GL_BOOL_VEC4,
+    FLOAT_MAT2​ = GL_FLOAT_MAT2,
+    FLOAT_MAT3​ = GL_FLOAT_MAT3,
+    FLOAT_MAT4​ = GL_FLOAT_MAT4,
+    FLOAT_MAT2x3​ = GL_FLOAT_MAT2x3,
+    FLOAT_MAT2x4​ = GL_FLOAT_MAT2x4,
+    FLOAT_MAT3x2​ = GL_FLOAT_MAT3x2,
+    FLOAT_MAT3x4​ = GL_FLOAT_MAT3x4,
+    FLOAT_MAT4x2​ = GL_FLOAT_MAT4x2,
+    FLOAT_MAT4x3​ = GL_FLOAT_MAT4x3,
+    DOUBLE_MAT2​ = GL_DOUBLE_MAT2,
+    DOUBLE_MAT3​ = GL_DOUBLE_MAT3,
+    DOUBLE_MAT4​ = GL_DOUBLE_MAT4,
+    DOUBLE_MAT2x3​ = GL_DOUBLE_MAT2x3,
+    DOUBLE_MAT2x4​ = GL_DOUBLE_MAT2x4,
+    DOUBLE_MAT3x2​ = GL_DOUBLE_MAT3x2,
+    DOUBLE_MAT3x4​ = GL_DOUBLE_MAT3x4,
+    DOUBLE_MAT4x2​ = GL_DOUBLE_MAT4x2,
+    DOUBLE_MAT4x3​ = GL_DOUBLE_MAT4x3,
+    SAMPLER_1D​ = GL_SAMPLER_1D,
+    SAMPLER_2D​ = GL_SAMPLER_2D,
+    SAMPLER_3D​ = GL_SAMPLER_3D,
+    SAMPLER_CUBE​ = GL_SAMPLER_CUBE,
+    SAMPLER_1D_SHADOW​ = GL_SAMPLER_1D_SHADOW,
+    SAMPLER_2D_SHADOW​ = GL_SAMPLER_2D_SHADOW,
+    SAMPLER_1D_ARRAY​ = GL_SAMPLER_1D_ARRAY,
+    SAMPLER_2D_ARRAY​ = GL_SAMPLER_2D_ARRAY,
+    SAMPLER_1D_ARRAY_SHADOW​ = GL_SAMPLER_1D_ARRAY_SHADOW,
+    SAMPLER_2D_ARRAY_SHADOW​ = GL_SAMPLER_2D_ARRAY_SHADOW,
+    SAMPLER_2D_MULTISAMPLE​ = GL_SAMPLER_2D_MULTISAMPLE,
+    SAMPLER_2D_MULTISAMPLE_ARRAY​ = GL_SAMPLER_2D_MULTISAMPLE_ARRAY,
+    SAMPLER_CUBE_SHADOW​ = GL_SAMPLER_CUBE_SHADOW,
+    SAMPLER_BUFFER​ = GL_SAMPLER_BUFFER,
+    SAMPLER_2D_RECT​ = GL_SAMPLER_2D_RECT,
+    SAMPLER_2D_RECT_SHADOW​ = GL_SAMPLER_2D_RECT_SHADOW,
+    INT_SAMPLER_1D​ = GL_INT_SAMPLER_1D,
+    INT_SAMPLER_2D​ = GL_INT_SAMPLER_2D,
+    INT_SAMPLER_3D​ = GL_INT_SAMPLER_3D,
+    INT_SAMPLER_CUBE​ = GL_INT_SAMPLER_CUBE,
+    INT_SAMPLER_1D_ARRAY​ = GL_INT_SAMPLER_1D_ARRAY,
+    INT_SAMPLER_2D_ARRAY​ = GL_INT_SAMPLER_2D_ARRAY,
+    INT_SAMPLER_2D_MULTISAMPLE​ = GL_INT_SAMPLER_2D_MULTISAMPLE,
+    INT_SAMPLER_2D_MULTISAMPLE_ARRAY​ = GL_INT_SAMPLER_2D_MULTISAMPLE_ARRAY,
+    INT_SAMPLER_BUFFER​ = GL_INT_SAMPLER_BUFFER,
+    INT_SAMPLER_2D_RECT​ = GL_INT_SAMPLER_2D_RECT,
+    UNSIGNED_INT_SAMPLER_1D​ = GL_UNSIGNED_INT_SAMPLER_1D,
+    UNSIGNED_INT_SAMPLER_2D​ = GL_UNSIGNED_INT_SAMPLER_2D,
+    UNSIGNED_INT_SAMPLER_3D​ = GL_UNSIGNED_INT_SAMPLER_3D,
+    UNSIGNED_INT_SAMPLER_CUBE​ = GL_UNSIGNED_INT_SAMPLER_CUBE,
+    UNSIGNED_INT_SAMPLER_1D_ARRAY​ = GL_UNSIGNED_INT_SAMPLER_1D_ARRAY,
+    UNSIGNED_INT_SAMPLER_2D_ARRAY​ = GL_UNSIGNED_INT_SAMPLER_2D_ARRAY,
+    UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE​ = GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE,
+    UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE_ARRAY​ = GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE_ARRAY,
+    UNSIGNED_INT_SAMPLER_BUFFER​ = GL_UNSIGNED_INT_SAMPLER_BUFFER,
+    UNSIGNED_INT_SAMPLER_2D_RECT​ = GL_UNSIGNED_INT_SAMPLER_2D_RECT
+  };
 
   /// Easy to use wrapper for OpenGL shader.
   class ShaderObject
   {
   public:
+    template<typename VariableType>
+    struct BufferInfo
+    {
+      ezString  sName;
+      GLint     iBufferBinding;       ///< OpengGL buffer binding index
+      ezInt32   iBufferDataSizeByte;  ///< Minimal buffer size in bytes
+
+      /// Known contained variable information
+      ezDynamicArray<VariableType> Variables;
+
+      // possible, but currently missing:
+      // - usage by shader stage (GL_REFERENCED_BY_..)
+    };
+
+    struct ShaderVariableInfoBase
+    {
+      ezString            sName;
+      ShaderVariableType  Type;         ///< Type
+      ezInt32             iBlockOffset; ///< Offset in corresponding buffer, -1 if there's no buffer
+
+      ezInt32 iArrayElementCount;   ///< Number of array elements
+      ezInt32 iArrayStride; ///< Stride between array elements in bytes
+      
+      ezInt32 iMatrixStride;  ///< stride between columns of a column-major matrix or rows of a row-major matrix
+      bool    bRowMajor;
+
+      // possible, but currently missing:
+      // - usage by shader stage (GL_REFERENCED_BY_..)
+    };
+
+    struct UniformInfo : ShaderVariableInfoBase
+    {
+      GLint iLocation;      ///< OpenGL location, -1 if used in a buffer
+      GLint iAtomicCounterbufferIndex; ///< index of active atomic counter buffer containing this variable​. -1 if this is no atomic counter buffer
+    };
+
+    struct BufferVariableInfo : ShaderVariableInfoBase
+    {
+      ezInt32 iTopLevelArraySize;
+      ezInt32 iTopLevelArrayStride;
+    };
+
     enum class ShaderType
     {
       VERTEX,
@@ -35,23 +154,58 @@ namespace gl
     ///         -> then you also can acitvate multiple programs at a time because they only overwrite the stages they use
     void Activate() const;
 
-  private:
-    // Print information about the compiling step
-    void PrintShaderInfoLog(GLuint shader, const ezString& sShaderName) const;
-    // Print information about the linking step
-    void PrintProgramInfoLog(GLuint program) const;
+    /// The set of active user-defined inputs to the first shader stage in this program. 
+    /// 
+    /// If the first stage is a Vertex Shader, then this is the list of active attributes.
+    /// If the program only contains a Compute Shader, then there are no inputs.
+    GLint GetTotalProgramInputCount() const   { return m_iTotalProgramInputCount; }
 
+    /// The set of active user-defined outputs from the final shader stage in this program.
+    /// 
+    /// If the final stage is a Fragment Shader, then this represents the fragment outputs that get written to individual color buffers.
+    /// If the program only contains a Compute Shader, then there are no outputs.
+    GLint GetTotalProgramOutputCount() const  { return m_iTotalProgramOutputCount; }
+
+  private:
+    /// Print information about the compiling step
+    static void PrintShaderInfoLog(GLuint shader, const ezString& sShaderName);
+    /// Print information about the linking step
+    static void PrintProgramInfoLog(GLuint program);
+
+    /// queries uniform informations from the program
+    void QueryProgramInformations();
+
+    /// Intern helper function for gather general BufferInformations
+    template<typename BufferVariableType>
+    void QueryBlockInformations(ezDynamicArray<BufferInfo<BufferVariableType>>& BufferToFill, GLenum InterfaceName);
+
+    // the program itself
     GLuint m_Program;
     bool m_ContainsAssembledProgram;
 
+    // underlying shaders
     struct Shader
     {
-      GLuint m_ShaderObject;
-      ezString m_sOrigin;
-      bool m_bLoaded;
+      GLuint    iGLShaderObject;
+      ezString  sOrigin;
+      bool      bLoaded;
     };
-
     Shader m_aShader[ShaderType::NUM_SHADER_TYPES];
+
+    // meta information
+    ezDynamicArray<UniformInfo> m_GlobalUniformInfo;
+    ezDynamicArray<BufferInfo<UniformInfo>>         m_UniformBlockInfos;
+    ezDynamicArray<BufferInfo<BufferVariableInfo>>  m_ShaderStorageInfos;
+
+    // misc
+    GLint m_iTotalProgramInputCount;  ///< \see GetTotalProgramInputCount
+    GLint m_iTotalProgramOutputCount; ///< \see GetTotalProgramOutputCount
+
+    // currently missing meta information
+    // - transform feedback buffer
+    // - transform feedback varying
+    // - subroutines
+    // - atomic counter buffer
   };
 
 }
